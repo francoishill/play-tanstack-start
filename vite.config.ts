944dac472334decec18ecd6ff026898764991229
Francois Hill <francoishill11@gmail.com>
2025-11-01 16:14:43 +0200

    wip: attempt to fix build on vercel followed hosting docs for nitro as recommended by tanstack start https://tanstack.com/start/latest/docs/framework/react/guide/hosting#nitro

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
@@ -12,6 +13,7 @@ export default defineConfig({
       projects: ["./tsconfig.json"],
     }),
     tanstackStart(),
+    nitro(),
     viteReact(),
   ],
 });
